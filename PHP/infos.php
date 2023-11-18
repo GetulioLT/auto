021a47c2ea72cc7581cd0cfa6bca6ba7e9054358
GetulioLT <88693489+GetulioLT@users.noreply.github.com>
2023-11-18 10:10:03 -0300

    Create infos.php

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "concessionaria.cyp6jjti2v9z.sa-east-1.rds.amazonaws.com";
+$username = "jason";
+$password = "hCM8ZRD9QgmIGel7x9h8";
+$database = "db_conce";
+?>
